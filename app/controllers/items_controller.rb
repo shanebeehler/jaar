17c06e07530735024090d7d2ec5d3fbf53b4e187
Shane <shanebeehler@me.com>
2016-09-26 17:05:02 -0400

    created items controller action methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,46 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if Item.save
+      redirect_to items_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update_attributes(item_params)
+      redirect_to items_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    redirect_to items_path
+  end
+
 end
