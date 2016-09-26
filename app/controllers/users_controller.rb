4e9b6d2ebfc7d2bc6dd027ff92cd1a70bcaadf35
Jack WIlde <jackwilde630@gmail.com>
2016-09-26 16:37:47 -0400

    begin sorcery integration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+  end
+
+end
