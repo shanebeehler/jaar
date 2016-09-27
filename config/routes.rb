ed542aff5c3ced5a31c09806223739230e4e5cad
Jack WIlde <jackwilde630@gmail.com>
2016-09-26 23:23:33 -0400

    reset password controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'password_resets/create'
+
+  get 'password_resets/edit'
+
+  get 'password_resets/update'
+
   root 'users#show'
   resource :users
   resources :user_sessions
