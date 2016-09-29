dfb88689c65f49ff961af590b0925e275928b5fb
Shane <shanebeehler@me.com>
2016-09-29 15:29:53 -0400

    fade out new jar modal on body click

--- app/assets/javascripts/jars.js
+++ app/assets/javascripts/jars.js
@@ -2,8 +2,8 @@ $(function () {
   $('#new-jar').on('click', function(event) {
     event.preventDefault();
     $('#new-jar-modal').fadeIn();
-
-
   });
-
+  $('#new-jar-modal').on('click', function() {
+    $('#new-jar-modal').fadeOut();
+  });
 });
