b38eac0c1844fa43107d9f2f1fb543a3afa48c8e
Jack WIlde <jackwilde630@gmail.com>
2016-09-30 09:05:56 -0400

    Ajax request for random jar item works!

--- app/assets/javascripts/jars.js
+++ app/assets/javascripts/jars.js
@@ -7,36 +7,26 @@ $(function () {
     $('#new-jar-modal').fadeOut();
   });
 
-  // $('.jar a').on('click', function(event) {
-  //   event.preventDefault();
-  // });
+  $('.jar a').on('click', function(event) {
+    event.preventDefault();
+  });
 
-//   $('.jar').on('click', function() {
-//     $.ajax({
-//       url: $(this).find('a').attr('href'),
-//       method: 'GET',
-//       data: {},
-//       dataType: 'json'
-//     }).done(function(responseData) {
-//       var jar = responseData[0];
-//       var items = responseData[1];
-//       var modal = $('<div class="modall">');
-//       $('<h1>').html(jar['name']).appendTo(modal);
-//
-//       if ( items.length === 0 ) {
-//         var content = $('<p>').html('This jar is empty!');
-//       } else {
-//         var randomItem = items[Math.floor(Math.random()*items.length)];
-//         if ( randomItem['type_id'] === 1 ) {
-//           var content = $('<p>').html(randomItem['comment']);
-//         } else if ( randomItem['type_id'] === 2 ){
-//           var content = $('<img>').attr('src', "/public/randomI['data_type_file_name'])
-//         }
-//       }
-//       modal.append(content);
-//       $('body').append(modal);
-//
-//
-//     });
-//   });
+  $('.jar').on('click', function() {
+    $.ajax({
+      url: $(this).find('a').attr('href'),
+      method: 'GET',
+      data: {},
+      dataType: 'json'
+    }).done(function(responseData) {
+      div = $('<div>')
+
+      if (responseData[0] === 1) {
+        $('<p>').html(responseData[1]).appendTo(div)
+      } else if (responseData[0] === 2) {
+        $('<img>').attr('src', responseData[1]).appendTo(div)
+      }
+
+      $('body').append(div)
+    });
+  });
  });
