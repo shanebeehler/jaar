089d23391043f1313939cf82a07a4c1a5830235b
Jack WIlde <jackwilde630@gmail.com>
2016-10-15 17:25:10 -0400

    new item modal is the correct color

--- app/assets/javascripts/jars.js
+++ app/assets/javascripts/jars.js
@@ -86,6 +86,9 @@ $(function () {
       $('#jar-modal > .modal-content').removeClass('color1 color2 color3 color4 color5 color6 color7')
       $('#jar-modal > .modal-content').addClass($(this).attr('class'));
       $('#jar-modal > .modal-content').removeClass('jar');
+      $('#new-item-modal > .modal-content').removeClass('color1 color2 color3 color4 color5 color6 color7')
+      $('#new-item-modal > .modal-content').addClass($(this).attr('class'));
+      $('#new-item-modal > .modal-content').removeClass('jar');
       $.ajax({
         url: $(this).find('a').attr('href'),
         method: 'GET',
